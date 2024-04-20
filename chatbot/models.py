b22ad0fa4e4e5e5372e30ede76ab661feaf44539
ssetsofia <setsofia@washnote.com>
2024-04-20 11:48:01 +0000

    changes

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,11 +1,4 @@
 from django.db import models
 
-class EconomicIndicator(models.Model):
-    name = models.CharField(max_length=100)
-    country = models.CharField(max_length=50)
-    value = models.FloatField()
-    year = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.name} ({self.country} - {self.year})"
 
